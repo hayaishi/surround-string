d484ac0af2c62fc6d0f806902e8737be459bccda
akihiro.hayaishi <akihiro.hayaishi@gmail.com>
2010-11-07 02:42:44 +0900

    add emacs-lisp file.

---
+++ surround-string.el
@@ -0,0 +1,48 @@
+;-*- Emacs-Lisp -*-
+
+(defun surround-string (surround-string)
+  (interactive)
+  (lexical-let* ((surround-list (split-string surround-string "|"))
+                 (begin-string (car surround-list))
+                 (end-string (cadr surround-list)))
+    (lambda ()
+      (interactive)
+      (let* ((befor-point-max (point-max))
+             (begin (if mark-active
+                        (region-beginning)
+                      (progn
+                        (mark-text-object)
+                        (region-beginning))))
+             (end (point))
+             (match-rgx (mapconcat 'identity (list "^" begin-string ".*?" end-string "$") ""))
+             (replace-rgx (mapconcat
+                           'identity
+                           (list "^\\(" begin-string "\\)\\(.*?\\)\\(" end-string "\\)$") ""))
+             (select-string (buffer-substring-no-properties begin end)))
+        (if (null (string-match match-rgx select-string))
+            (progn
+              (goto-char end)
+              (insert end-string)
+              (goto-char begin)
+              (insert begin-string))
+          (progn
+            (setq select-string (replace-regexp-in-string replace-rgx "\\2" select-string))
+            (delete-region begin end)
+            (goto-char begin)
+            (insert select-string)))
+        (goto-char (+ end (- (point-max) befor-point-max)))))))
+
+(defun mark-text-object ()
+  (interactive)
+  (let* ((current-point (point))
+         (begin (if (re-search-backward " \\|\n")
+                    (goto-char (+ (point) 1))
+                  (point-min)))
+         (end (if (re-search-forward " \\|\n")
+                    (goto-char (- (point) 1))
+                (point-max))))
+    (progn
+      (push-mark begin t transient-mark-mode)
+      (goto-char end))))
+
+(provide 'surround-string)
